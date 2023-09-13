0e99e1afd55a90cb90f0b4c7c55d667eb44fd334
Indra Prakoso <inprapoe@gmail.com>
2023-09-13 08:29:21 +0700

    fixing loading

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FaSpinner } from 'react-icons/fa'
 import FullPage from '../FullPage/FullPage'
-import { motion } from "framer-motion"
 
 interface Props {
   label?: string
@@ -10,31 +9,14 @@ interface Props {
 export default function Loading({ label }: Props) {
   return (
     <FullPage>
-      <motion.div 
-        className='flex items-center flex-col'
-        initial="hidden"
-        animate="show"
-        variants={{
-          hidden: {
-            opacity: 0,
-            translateY: 10
-          },
-          show: {
-            opacity: 1,
-            translateY: 0
-          }
-        }}
-        
-      >
-        <div className="mb-5">
-          <FaSpinner className={'text-5xl animate-spin text-blue-700'}/>
-        </div>
-        {label && (
-          <span>
-            {label}
-          </span>
-        )}
-      </motion.div>      
+      <div className="mb-5">
+        <FaSpinner className={'text-5xl animate-spin text-blue-700'}/>
+      </div>
+      {label && (
+        <span>
+          {label}
+        </span>
+      )}   
     </FullPage>
   )
 }
